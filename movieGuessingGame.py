340d9c3a4965f93c5e1b7202a726a566ec5357f1
Benjamin Godfrey <ben.d.godfrey2002@gmail.com>
2025-03-19 23:20:14 +0000

    add blurb redaction

--- movieGuessingGame.py
+++ movieGuessingGame.py
@@ -12,7 +12,7 @@ class GuessingGame:
         self.movie = random.choice(self.movieList)
 
         # Generate movie blurb
-        prompt = "Generate a blurb description of the movie " + self.movie+ ". The description should be about 5 sentences long. Give me the text of this description and nothing else."
+        prompt = "Generate a blurb description of the movie " + self.movie+ ". The description should be about 5 sentences long and should not explicitly say the names of any people or places. Give me the text of this description and nothing else."
         blurbResponse = client.models.generate_content(
             model="gemini-2.0-flash",
             contents=prompt
@@ -21,4 +21,23 @@ class GuessingGame:
 
         print(self.blurbText)
 
+        # Decide which characters should be redacted and which should not
+        # All should be at first
+        self.redactedList = [True for i in range(len(self.blurbText))]
+    
+    def printRedacted(self):
+        # Print the blurb with the chosen characters redacted
+        redactedString = "".join("-" if redacted else char for char, redacted in zip(self.blurbText, self.redactedList))
+        print(redactedString)
+
+    def updateRedactedList(self):
+        # Make about 20% of the currently redacted characters unredacted
+        for i in range(len(self.redactedList)):
+            if self.redactedList[i]:
+                if random.random() < 0.2:
+                    self.redactedList[i] = False
+
 game = GuessingGame()
+for i in range(10):
+    game.printRedacted()
+    game.updateRedactedList()
